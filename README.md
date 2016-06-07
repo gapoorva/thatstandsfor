1bba69e685b9e1ec616c13529d310191e6eaf05a
gapoorva <gapoorva@umich.edu>
2016-06-06 19:05:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# That Stand For
+
+Finally! A useful Slack integration that will tell you what those pesky acronyms mean! 
+
+Disclaimer: Not actually intended to return the correct contextual meaning of requested acronyms. This is mostly on purpose.
+
+Setup instructions:
+
+1. Find the settings on your Slack to add a custom slash command. Or just follow this link: [Add a Slash Command](https://my.slack.com/services/new/slash-commands)
+
+2. Enter `/what's` for the command name. (You can actually pick whatever you want but if you make it `what's`, it's a lot more fun.)
+
+3. 
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	if($_SERVER['REQUEST_METHOD'] == "GET") {
+?>
+
+<html>
+	<head>
+		<title>A Slack integration</title>
+		<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container">
+		  <div class="jumbotron">
+		    <h1>That Stands For</h1> 
+		    <p>A Slack integration that tells you what acryonms mean. Mostly used to mock people that overuse acryonms.</p> 
+		    <p>More info and installation instructions <a href="http://github.com/gapoorva/thatstandsfor"> here. </a></p>
+		  </div>
+		</div>
+	</body> 
+</html>
+<?php
+	}
+?>
